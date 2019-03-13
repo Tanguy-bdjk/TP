a8cf9d395c51853e7c88f96036c593decb47baa2
tanguy <tanguybondjokwe@gmail.com>
2019-03-13 14:34:03 +0100

    TP

---
+++ Exo1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exo1
+{
+    class Program
+    {
+        private static object imc;
+
+        static void Main(string[] args)
+        {
+            NameValueCollection imc = new NameValueCollection();
+            imc.Add("Moins de 16,5", "Dénutrition");
+            imc.Add("16,5 à 18.5", "Maigreur ");
+            imc.Add("18,5 à 25", "Corpulence normale");
+            imc.Add("25 à 30", "Surpoids");
+            imc.Add("30 à 35", "Obésité modérée ");
+            imc.Add("35 à 40", "Obésité sévère");
+            imc.Add("plus de 40", "Obésitè morbide ou massive");
+
+
+            double poids = 0;
+            double taille = 0;
+            Console.WriteLine("votre poids ?");
+            double.TryParse(Console.ReadLine(), out poids);
+            Console.WriteLine("votre taille ?");
+            double.TryParse(Console.ReadLine(), out taille);
+
+            imc = Math.Pow(poids) / Math.Pow(taille, 2);
+        
+            if
+
+
+        }
+    }
+}
---
+++ Exo2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_pour_tanguy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();//Utilisation de la fonction random.
+            int random = rand.Next(51);//L'ordinateur choisi un nombre aléatoire entre 1 et 50 .
+            int choice = 0, compt = 0, maxcompt = 0;
+
+            Console.WriteLine("Definir un nombre de coup maximum pour cette partie !");
+            int.TryParse(Console.ReadLine(), out maxcompt);
+
+            while (compt < maxcompt)
+            {
+                Console.WriteLine("\nDevine un nombre ");
+                int.TryParse(Console.ReadLine(), out choice);
+                compt++;
+
+                if (choice < random && compt < maxcompt)
+                {
+                    Console.WriteLine("Faux, essaye encore , c'est plus grand ! Attention, il te reste {0} coups!!!", maxcompt - compt);
+                }
+                else if (choice > random && compt < maxcompt)
+                {
+                    Console.WriteLine("Faux, essaye encore , c'est plus petit ! Attention, il te reste {0} coups!!!", maxcompt - compt);
+                }
+                else if (choice == random)
+                {
+                    Console.WriteLine("Félicitation vous avez gagné en {0} coups !!!  ", compt);
+                    compt = maxcompt;
+                }
+                else
+                {
+                    Console.WriteLine("Vous avez perdu, le nombre mystère été {0}", random);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
